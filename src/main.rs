aed9a6c7d769207d8e98bc49149e38f45b9aaec4
Barend <bvanrijn@users.noreply.github.com>
2018-06-15 23:27:32 +0200

    Add resize module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate serde_json;
 extern crate image;
 extern crate color_thief;
 extern crate rgb;
+extern crate image_utils;
+extern crate tempfile;
+
+use image::GenericImage;
 
 #[macro_use]
 extern crate serde_derive;
@@ -10,9 +14,11 @@ extern crate serde_derive;
 mod colordifference;
 mod datavalues;
 mod dominant;
+mod resize;
 
 fn main() {
     println!("{}", colordifference::color_difference(&mut [0, 0, 0], &mut [255, 255, 255]));
     println!("{:?}", datavalues::get());
     println!("{:?}", dominant::get_color("items/images/1-0.png".to_string()));
+    println!("{:?}", resize::resize("President_Barack_Obama.jpg".to_string()).dimensions());
 }
---
+++ src/resize/mod.rs
@@ -0,0 +1,18 @@
+use image;
+use image_utils;
+use std::path::Path;
+use tempfile;
+
+pub fn resize(file_name: String) -> image::DynamicImage {
+    let temp_dir = tempfile::tempdir().unwrap();
+    let output_file = &temp_dir.path().join(".resized.jpg");
+
+    image_utils::resize(
+        &Path::new(&file_name),
+        50,
+        50,
+        &Path::new(output_file),
+    ).unwrap();
+
+    return image::open(output_file).unwrap();
+}
